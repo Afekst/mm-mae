788be0c82337a4cca1f7e53ecb24f50a93e4e7b2
Afek Steinberg <afeks98@outlook.com>
2025-07-07 16:40:04 +0300

    auto-format to pass CI

--- src/mm_mae/train.py
+++ src/mm_mae/train.py
@@ -1,12 +1,15 @@
 # src/mm_mae/train.py
-import hydra 
+import hydra
 import pytorch_lightning as pl
 from omegaconf import DictConfig
+
 
 @hydra.main(version_base=None, config_path="../../configs/train", config_name="default")
 def main(cfg: DictConfig):
     pl.seed_everything(cfg.seed)
     # TODO: replace with real datamodule & model
     print("Config looks good:", cfg)
+
+
 if __name__ == "__main__":
     main()
--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,5 +1,6 @@
 # tests/test_smoke.py
 from importlib import import_module
 
+
 def test_import():
     assert import_module("mm_mae") is not None
